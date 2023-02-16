5887cebc4150764cc8b6da6abd50e75cb1cf0142
Senpai9000 <bradleygilden@gmail.com>
2023-02-16 10:43:54 +0200

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,10 @@
 
 int main(void)
 {
-        printf("Hello World!");
-        return (0);
+	printf("Size of char: %ld byte(s)\n", sizeof(char));
+	printf("Size of int: %ld byte(s)\n", sizeof(int));
+	printf("Size of long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of float: %ld byte(s)\n", sizeof(float));
+	return (0);
 }
