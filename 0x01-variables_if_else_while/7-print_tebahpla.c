292fe8f5ce0079a0357ff9d653c0ef12b27849a1
Senpai9000 <bradleygilden@gmail.com>
2023-02-17 09:04:42 +0200

    reverse and hex

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,6 +8,12 @@
 
 int main(void)
 {
-	printf("Hello World!");
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,6 +8,19 @@
 
 int main(void)
 {
-	printf("Hello World!");
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 97; i <= 102; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
 	return (0);
 }
