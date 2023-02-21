2faf67748b30b512686ee5381cc2451368b42bc4
Senpai9000 <bradleygilden@gmail.com>
2023-02-21 09:29:33 +0200

    ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - is the entry point for program
- *
- * Return: 0 if compilation is successful
+ * print_alphabet_x10 - prints alphabet 10 times
  */
 
-int main(void)
+void print_alphabet_x10(void)
 {
-	printf("Hello World!");
-	return (0);
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i < 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
 }
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -9,6 +9,6 @@
 
 int main(void)
 {
-	print_alphabet();
+	print_alphabet_x10();
 	return (0);
 }
