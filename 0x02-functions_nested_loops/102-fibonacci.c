50d84bd79b6ddcde702e028733cae6565372b27b
Senpai9000 <bradleygilden@gmail.com>
2023-02-21 17:44:59 +0200

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,11 +3,30 @@
 /**
  * main - is the entry point for program
  *
- * Return: 0 if compilation is successful
+ * Return: 0 always
  */
 
 int main(void)
 {
-	printf("Hello World!");
+	int i, j;
+	long int arr[50];
+
+	j = 0;
+
+	for (i = 1; i < 51; i++)
+	{
+		if (i > 2)
+			arr[j] = arr[j - 2] + arr[j - 1];
+		else
+			arr[j] = i;
+		j++;
+	}
+	for (i = 0; i < 50; i++)
+	{
+		if (i < 49)
+			printf("%ld, ", arr[i]);
+		else
+			printf("%ld\n", arr[i]);
+	}
 	return (0);
 }
