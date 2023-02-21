cd3a5e1211f70fbd65281857dbe34d940190f1be
Senpai9000 <bradleygilden@gmail.com>
2023-02-21 12:17:41 +0200

    fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,30 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - is the entry point for program
+ * jack_bauer - print 24 hours
  *
- * Return: 0 if compilation is successful
  */
 
-int main(void)
+void jack_bauer(void)
 {
-	printf("Hello World!");
-	return (0);
+	int i, j, k, z;
+
+	for (i = '0'; i <= '2'; i++)
+	{
+	for (j = '0'; i <= '3'; i++)
+	{
+		for (k = '0'; i <= '5'; i++)
+		{
+			for (z = '0'; i <= '9'; i++)
+			{
+				_putchar(i);
+				_putchar(j);
+				_putchar(':');
+				_putchar(k);
+				_putchar(z);
+				_putchar('\n');
+			}
+}
+}
+}
 }
