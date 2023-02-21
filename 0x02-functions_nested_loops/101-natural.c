3b16d543906432f926da213d23f9a71667734054
Senpai9000 <bradleygilden@gmail.com>
2023-02-21 17:14:52 +0200

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,11 +3,20 @@
 /**
  * main - is the entry point for program
  *
- * Return: 0 if compilation is successful
+ * Return: 0 always
  */
 
 int main(void)
 {
-	printf("Hello World!");
+	int i, count;
+
+	count = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			count += i;
+	}
+	printf("%d\n", count);
 	return (0);
 }
