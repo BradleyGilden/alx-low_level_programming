35f8c192cfb28a24d286dfa48a2ee1554dd6480d
Senpai9000 <bradleygilden@gmail.com>
2023-02-21 10:07:13 +0200

    check alphabets

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - is the entry point for program
+ * _isalpha - is the entry point for program
+ * @c: integer ascii value
  *
- * Return: 0 if compilation is successful
+ * Return: 1 if letter is lowercase or uppercase, 0 otherwise
  */
 
-int main(void)
+int _isalpha(int c)
 {
-	printf("Hello World!");
-	return (0);
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
 }
