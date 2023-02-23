d19e59eda14903cd100b51467e7f387ed899636a
Senpai9000 <bradleygilden@gmail.com>
2023-02-23 11:14:34 +0200

    remove 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,19 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - entry point
- *
- * Return: 0 always
+ * print_most_numbers - entry point
  */
 
-int main(void)
+void print_most_numbers(void)
 {
-	return (0);
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == '2' || i == '4')
+			continue;
+		_putchar(i);
+	}
+	_putchar('\n');
 }
