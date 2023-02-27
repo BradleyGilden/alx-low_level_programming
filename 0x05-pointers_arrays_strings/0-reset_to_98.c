c5143a2e77a2eba4a839b568d92d83f2dfe1ad8c
Senpai9000 <bradleygilden@gmail.com>
2023-02-27 08:40:05 +0200

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - set argument to 98
+ * @n: a pointer to integer argument
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	return (0);
+}
