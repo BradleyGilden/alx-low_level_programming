51f87f8cdc16f5958ff012eb4dbbc6b2f009f97e
Senpai9000 <bradleygilden@gmail.com>
2023-02-27 21:46:58 +0200

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - convert string to int
+ * @s: input string
+ *
+ * Return: int value
+*/
+
+int _atoi(char *s)
+{
+	int i, value = 0, sign = 1, end = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			sign = (-1) * sign;
+
+		if (end > 0 && s[i] == ' ')
+			break;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+
+			value = value * 10 + s[i] - '0';
+			end += 1;
+		}
+
+	}
+	value = value * sign;
+	return (value);
+}
