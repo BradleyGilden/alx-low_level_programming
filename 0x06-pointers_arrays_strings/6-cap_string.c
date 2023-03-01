bfcbe22975c3e74ef0837d7ecf8d812d05774cbd
Senpai9000 <bradleygilden@gmail.com>
2023-03-01 22:02:56 +0200

    capitalise

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,12 +2,40 @@
 #include "main.h"
 
 /**
- * main - entry point
- *
- * Return: 0 always
- */
+  * cap_string - captilize words divided by special chars
+  * @str: input string
+  *
+  * Return: address of input str
+  */
 
-int main(void)
+char *cap_string(char *str)
 {
-	return (0);
+	int i = 0, reset = 0;
+
+	while (str[i])
+	{
+		if (i == 0 && str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
+
+		if (reset >= 1 && str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] -= 32;
+			reset = 0;
+		}
+
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',')
+			reset++;
+		else if (str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?')
+			reset += 1;
+		else if (str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{')
+			reset++;
+		else if (str[i] == '}')
+			reset++;
+		else if (!(str[i] >= 'a' && str[i] <= 'z'))
+			reset = 0;
+
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
