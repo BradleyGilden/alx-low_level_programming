822ddd6dd27d5314cd004d104dc15af3d4d35481
Senpai9000 <bradleygilden@gmail.com>
2023-03-01 19:34:14 +0200

    uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,12 +2,22 @@
 #include "main.h"
 
 /**
- * main - entry point
+ * string_toupper - convert lowercase to uppercase
+ * @str: string input
  *
- * Return: 0 always
+ * Return: adress of capitalized string
  */
 
-int main(void)
+char *string_toupper(char *str)
 {
-	return (0);
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
