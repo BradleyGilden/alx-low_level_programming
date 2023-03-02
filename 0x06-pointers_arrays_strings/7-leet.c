715f08f894faeab84e93d9828512e0903391cc94
Senpai9000 <bradleygilden@gmail.com>
2023-03-02 08:12:27 +0200

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,7 +10,7 @@
 
 char *leet(char *str)
 {
-	int i = 0, j = 0, len = 0;
+	int i = 0, j = 0;
 	int code[8] = {'O', 'L', '~', 'E', 'A', '~', '~', 'T'};
 
 	/*             0    1    2    3    4    5    6    7        */
@@ -26,4 +26,15 @@ char *leet(char *str)
 	}
 	str[i] = '\0';
 	return (str);
+}
+
+int main(void)
+{
+    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+    char *p;
+
+    p = leet(s);
+    printf("%s", p);
+    printf("%s", s);
+    return (0);
 }
