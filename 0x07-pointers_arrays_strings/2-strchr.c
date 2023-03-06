4a96cafa270e16b4318d326820b928858b936421
Senpai9000 <bradleygilden@gmail.com>
2023-03-06 12:14:08 +0200

    strchr fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -29,17 +29,3 @@ char *_strchr(char *s, char c)
 
 	return (NULL);
 }
-
-int main(void)
-{
-	char *s = "hello";
-	char *f;
-
-	f = _strchr(s, '\0');
-
-	if (f != NULL)
-	{
-		printf("%s\n", f);
-	}
-	return (0);
-}
