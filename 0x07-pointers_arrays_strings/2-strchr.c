48b18dfce54c0cd3ca43b1f20add84c37d399659
Senpai9000 <bradleygilden@gmail.com>
2023-03-06 12:13:12 +0200

    strchr fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,6 +4,7 @@
 /**
  * _strchr - returns pointer to first occurence of char c
  *			 if char c is not found then return NULL
+ *			 if c is '\0' then return pointer to end of string
  * @s: char array to be searched
  * @c: char needed to search in char array
  *
@@ -14,12 +15,31 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	for (i = 0; s[i]; i++)
+	while (s[i])
 	{
 		if (s[i] == c)
 		{
 			return (s + i);
 		}
+		i++;
 	}
+
+	if (c == '\0')
+		return (s + i);
+
 	return (NULL);
+}
+
+int main(void)
+{
+	char *s = "hello";
+	char *f;
+
+	f = _strchr(s, '\0');
+
+	if (f != NULL)
+	{
+		printf("%s\n", f);
+	}
+	return (0);
 }
