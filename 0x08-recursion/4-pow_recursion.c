c492df73fc61f2e52346c8862a4131f2b7684aa5
Senpai9000 <bradleygilden@gmail.com>
2023-03-08 12:59:05 +0200

    clone of pow() function using recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,12 +2,25 @@
 #include "main.h"
 
 /**
- * main - entry point
+ * _pow_recursion - returns the factorial of input n;
+ * @x: base
+ * @y: exponent
  *
- * Return: 0 always
+ * Return: x ^ y
+ *
+ * deduction: pow(x, 3) = x * x * x
+ *						= x * pow(x, 3 - 1)'
+ *			  lets use head recursion
+ *			  base case: if y = 0, return (1)
+ *			  exception: if y < 0 return (-1)
  */
 
-int main(void)
+int _pow_recursion(int x, int y)
 {
-	return (0);
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
 }
