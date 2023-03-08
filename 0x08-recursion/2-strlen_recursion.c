29f4e8e2091aae28a83d68aa291ee972c7d29c7e
Senpai9000 <bradleygilden@gmail.com>
2023-03-08 12:34:09 +0200

    betty approved

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -16,15 +16,6 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	
+
 	return (1 + _strlen_recursion(s + 1));
-}
-
-int main(void)
-{
-    int n;
-
-    n = _strlen_recursion("Corbin Coleman");
-    printf("%d\n", n);
-    return (0);
 }
