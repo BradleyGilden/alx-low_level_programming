e0b49a218fd8df758ab361bbcdb42627418d8793
Senpai9000 <bradleygilden@gmail.com>
2023-03-10 13:40:14 +0200

    infinite add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - entry point
+ * main - add argument numbers
+ *		  only digits allowed
+ * @argc: argument count
+ * @argv: argument vector
  *
  * Return: 0 always
  */
 
-int main(void)
+int main(int argc, char *argv[])
 {
+	int i, j, add = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				add += atoi(argv[i]);
+		}
+	}
+
+	printf("%d\n", add);
 	return (0);
+
 }
