21edaf5426f6b773e331347b2d6a28c67383c75c
Bradley Gilden <bradleygilden@gmail.com>
2023-03-13 22:15:53 +0200

    free fix

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -13,7 +13,13 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	for (i = height; i >= 0; i--)
-		free(grid[i]);
-	free(grid);
+	if (grid == NULL)
+		free(grid);
+	else
+	{
+		for (i = height; i >= 0; i--)
+			free(grid[i]);
+		free(grid);
+	}
+
 }
