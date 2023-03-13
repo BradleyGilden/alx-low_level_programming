fbb81cdf182ca0d506ae2d6f3a741ef037fc4723
Bradley Gilden <bradleygilden@gmail.com>
2023-03-13 22:12:08 +0200

    free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * main - entry point
+ * free_grid - free dynamically allocated 2d array
  *
- * Return: 0 always
+ * @grid: pointer to pointer of int (grid array input)
+ * @height: the number of rows of the grid
  */
 
-int main(void)
+void free_grid(int **grid, int height)
 {
-	return (0);
+	int i;
+
+	for (i = height; i >= 0; i--)
+		free(grid[i]);
+	free(grid);
 }
