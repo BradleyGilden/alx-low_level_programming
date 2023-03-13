8d43aff5d2e2344faf7a14a1ff75c84d47dbee63
Bradley Gilden <bradleygilden@gmail.com>
2023-03-13 13:30:42 +0200

    strdup function clone with malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * main - entry point
+ * _strdup - creates a copy of string input and stores it dynamically
+ *			 it then returns a pointer to the copy of the string.
  *
- * Return: 0 always
+ * @str: input string to be copied
+ *
+ * Return: NULL if str == NULL
+ *		   NULL if there is insufficient memeory
+ *
+ * deduction: first find length of string
+ *			  allocate memory in malloc for length + 1 to include null byte
+ *			  check if copied string :copystr, points to null
+ *			  if so return NULL
+ *			  assign str elements to copystr
  */
 
-int main(void)
+char *_strdup(char *str)
 {
-	return (0);
+	int i = 0, len = 0;
+	char *cpystr;
+
+	if (str == NULL)
+		return(NULL);
+
+	while (str[i++])
+		len++;
+
+	cpystr = (char*)malloc((len + 1) * sizeof(char));
+
+	if (cpystr == NULL)
+		return(NULL);
+
+	for (i = 0; i <= len; i++)
+		cpystr[i] = str[i];
+
+	return (cpystr);
 }
