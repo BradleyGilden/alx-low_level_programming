62fe5951e0cbdde67422669975bf1ca1653619cd
Bradley Gilden <bradleygilden@gmail.com>
2023-03-20 12:22:45 +0200

    checking documentation again

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,10 @@
 #ifndef _FUNCTION_LIKE_MACRO_H_
 #define _FUNCTION_LIKE_MACRO_H_
 
+/*
+ * Description: finds the absolute value of x
+ */
+
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif/*_FUNCTION_LIKE_MACRO_H_*/
