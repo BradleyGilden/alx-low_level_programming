632e663a5f0839884719f67c9d4c45aa929f48ff
Bradley Gilden <bradleygilden@gmail.com>
2023-03-22 08:28:33 +0200

    call function on every iteration of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - activate specific functions on every element of array
+ * @array: input array
+ * @size: size of array
+ * @action: function implemented on function elements
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || action == NULL || size < 0)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
