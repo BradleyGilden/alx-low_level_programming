25867c5dc48b2c647075d71e2550cf870221ddfc
Bradley Gilden <bradleygilden@gmail.com>
2023-03-22 12:33:51 +0200

    removed size comparison

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array == NULL || action == NULL || size < 0)
+	if (array == NULL || action == NULL)
 		return;
 
 	for (i = 0; i < size; i++)
