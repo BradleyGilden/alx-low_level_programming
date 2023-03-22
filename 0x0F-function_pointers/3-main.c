569719c2ae240febfe3eb0d0a2aaf0f6e83033b6
Bradley Gilden <bradleygilden@gmail.com>
2023-03-22 08:36:42 +0200

    entry point of the program

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+
+/**
+ * main - main function for the calculator
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 always
+ */
+
+int main(int ac, char *av[])
+{
+	int num1 = 0, num2 = 0, result = 0;
+	char op;
+
+	if (ac != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	op = av[2][0];
+	if (op != '+' && op != '-' && op != '*' && op != '/' && op != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	num1 = atoi(av[1]);
+	num2 = atoi(av[3]);
+
+	if ((op == '/' || op == '%') && (num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = get_op_func(av[2])(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
