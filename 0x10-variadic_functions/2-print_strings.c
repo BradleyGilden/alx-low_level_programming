4434495c385bb62205424761141ed02ee9284e23
Bradley Gilden <bradleygilden@gmail.com>
2023-03-23 12:21:34 +0200

    print strings with delimeter

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings followed by a new line
+ *				   if separator is NULL, don't print it
+ *				   if str arg is NULL, print "(nil)"
+ *
+ * @n: parameter count and last fixed argument
+ * @separator:: the string printed betweefn numbers
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+	char *str;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char *);
+
+		if (str == NULL)
+			str = "(nil)";
+
+		if (i == n - 1)
+		{
+			printf("%s\n", str);
+			break;
+		}
+		if (separator == NULL)
+		{
+			printf("%s", str);
+			continue;
+		}
+		printf("%s%s", str, separator);
+	}
+
+	va_end(ap);
+}
