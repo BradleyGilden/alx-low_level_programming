8b0e56e1cf2a6fbc7f3339662cd9aeb19a7c7299
Bradley Gilden <bradleygilden@gmail.com>
2023-03-23 16:50:40 +0200

    updated header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,25 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * struct print - this truct handles which function to call for a specific
+ *				  format specifier
+ * @spec: the format specifier print data as
+ * @func: the function needed print a specific format specifier
+ *
+ */
+
+typedef struct print
+{
+	char *spec;
+	void (*func)(va_list);
+} print_t;
+
+void s_print(va_list ap);
+void c_print(va_list ap);
+void i_print(va_list ap);
+void f_print(va_list ap);
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
