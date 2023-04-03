6885349aef374dadc0859419cf37bd2670ab4c8c
Bradley Gilden <bradleygilden@gmail.com>
2023-04-03 10:09:09 +0200

    free linked list with head pointing to  NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees linked list from memory with head=NULL
+ * @head: head pointer to first node
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *nxt_ptr = *head;
+	listint_t *ptr = *head;
+
+	while (nxt_ptr != NULL)
+	{
+		nxt_ptr = nxt_ptr->next;
+		free(ptr);
+		ptr = nxt_ptr;
+	}
+
+	*head = NULL;
+}
