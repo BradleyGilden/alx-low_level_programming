51b0c319cd8d8224b2ca131fe69f627b9a3d2a9c
Bradley Gilden <bradleygilden@gmail.com>
2023-04-06 22:12:26 +0200

    rewrite function to use bitwise operators

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,7 +10,8 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0, sum = 0, to_bin = 0;
+	unsigned int i = 0, num = 0x00, to_bin = 0;
+	int tracker = 0;
 
 	if (b == NULL)
 		return (0);
@@ -24,10 +25,26 @@ unsigned int binary_to_uint(const char *b)
 	i = 0;
 	while (b[i])
 	{
-		to_bin = (b[i] - '0');
-		sum = 2 * sum + to_bin;
+		if (b[i + 1])
+		{
+			if (b[i] == '1')
+			{
+				tracker = 1;
+				num |= 0x01;
+				num <<= 1;
+			}
+			else if (tracker == 1)
+				num <<= 1;
+		}
+		else
+		{
+			if (b[i] == '1')
+			{
+				num |= 0x01;
+			}
+		}
 		i++;
 	}
 
-	return (sum);
+	return (num);
 }
