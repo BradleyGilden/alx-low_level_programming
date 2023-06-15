cf091a5b31bbd361a4e96384c34ddfc54a43ddf3
BradleyGilden <bradleygilden@gmail.com>
2023-06-15 14:05:15 +0200

    sum of node data

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - return sum of data in doubly linked list
+ * @head: pointer to head node.
+ * Return: sum of data (n)
+ */
+int sum_dlistint(dlistint_t *head)
+{
+    int sum = 0;
+
+    while (head != NULL)
+    {
+        sum += head->n;
+        head = head->next;
+    }
+
+    return (sum);
+}
