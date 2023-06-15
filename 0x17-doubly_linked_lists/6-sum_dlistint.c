deef4de13e7e82c62a125284b704f90b485284dc
BradleyGilden <bradleygilden@gmail.com>
2023-06-15 14:06:25 +0200

    fix formatting

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -7,13 +7,13 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
-    int sum = 0;
+	int sum = 0;
 
-    while (head != NULL)
-    {
-        sum += head->n;
-        head = head->next;
-    }
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
 
-    return (sum);
+	return (sum);
 }
