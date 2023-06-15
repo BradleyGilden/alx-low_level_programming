8e28c4cfb440879396ee0afb9d91ed3c4151f534
BradleyGilden <bradleygilden@gmail.com>
2023-06-15 17:56:19 +0200

    updated back pointer to initialize to head

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -31,7 +31,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 	if (index == 0)
 		return (delete_beginning(head));
-
+	back = *head;
 	ptr = (*head)->next;
 	while (ptr != NULL)
 	{
