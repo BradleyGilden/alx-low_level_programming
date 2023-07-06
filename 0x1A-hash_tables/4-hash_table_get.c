8db5bed7e1129248bd462e6c501bbae1d3541b2b
BradleyGilden <bradleygilden@gmail.com>
2023-07-06 20:09:23 +0200

    updated error check

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,6 +11,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *ptr = NULL;
 
+	if (ht == NULL || key == NULL)
+		return NULL;
+
 	index = key_index((const unsigned char *)key, ht->size);
 
 	if (ht->array[index] == NULL)
