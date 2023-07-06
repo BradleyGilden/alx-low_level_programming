c27b5a63507c5c808ee99fee20e5669b88a72373
BradleyGilden <bradleygilden@gmail.com>
2023-07-06 20:11:20 +0200

    added parenthesis for return

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *ptr = NULL;
 
 	if (ht == NULL || key == NULL)
-		return NULL;
+		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
