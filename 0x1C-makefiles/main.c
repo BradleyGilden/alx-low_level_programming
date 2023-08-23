246fff7f1e1a20a0739bc6d5a9b0a7b92d5c0317
BradleyGilden <bradleygilden@gmail.com>
2023-08-23 11:20:31 +0200

    files to build

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "school.h"
+
+/**
+ * main - entry point
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	printf("%s", logo());
+
+	return (0);
+}
---
+++ 0x1C-makefiles/school.h
@@ -0,0 +1,6 @@
+#ifndef SCHOOL_H
+#define SCHOOL_H
+
+const char *logo(void);
+
+#endif /*SCHOOL_H*/
